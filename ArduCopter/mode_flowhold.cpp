34e4232ce5ed3c5cde092a7af9bbfd22bd974b79
Benjamin Castor <178317378+benjamincastor@users.noreply.github.com>
2025-03-21 09:12:06 -0400

    prevent flow hold over 19 m

--- ArduCopter/mode_flowhold.cpp
+++ ArduCopter/mode_flowhold.cpp
@@ -1,10 +1,27 @@
 #include "Copter.h"
 #include "defines.h"
-
+#include "AP_Baro/AP_Baro.h"       // Include Barometer header
+#include "AP_AHRS/AP_AHRS.h"       // AHRS for relative altitude
+extern Copter copter;
 #if MODE_FLOWHOLD_ENABLED == ENABLED
 
 bool ModeFlowHold::init(bool ignore_checks)
 {
+    // Fetch current altitude from the barometer (ASL)
+    float current_alt_m = AP::baro().get_altitude();
+
+    // Prevent mode switch if the altitude is too high
+    if (copter.motors->armed()){
+        float relative_alt_m = current_alt_m - copter.arming_altitude_m;
+        if (relative_alt_m >= 19.0f) {
+            gcs().send_text(MAV_SEVERITY_ALERT, "Altitude too high for flow mode");
+            gcs().send_text(MAV_SEVERITY_INFO, "Relative altitude above home = %.2f m", relative_alt_m);
+            return false;
+        }
+    } else {
+        gcs().send_text(MAV_SEVERITY_INFO, "Flow hold enabled at = %.2f m", current_alt_m);
+    }
+
     return ModeLoiter::init(ignore_checks);
 }
 
